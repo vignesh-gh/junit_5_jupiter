3c3624c762ba6aac276817720afcff990665ef3f
vigneshp <vigneshp@ELPBLR-049.evolvus.com>
2020-03-06 11:10:31 +0530

    New Test cases Added

--- src/test/java/com/vignesh/jupiter_junit5/BasicMathsTest.java
+++ src/test/java/com/vignesh/jupiter_junit5/BasicMathsTest.java
@@ -8,32 +8,64 @@ import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestReporter;
 
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class BasicMathsTest {
 	
+	TestInfo testInfo;
+	TestReporter testreporter;
+	
 	BasicMaths basicMaths;
 	
 	@BeforeEach
-	void init(){
+	void init(TestInfo testIn,TestReporter tr){
+		this.testInfo=testIn;
+		this.testreporter=tr;
 		basicMaths=new BasicMaths();
+		
+		
+		
+		
+	}
+
+	/*
+	 * @Test
+	 * 
+	 * @DisplayName("adding Two numbers") public void addNumberTest() { boolean
+	 * serverUp=true; assumeTrue(serverUp); assertAll( ()->assertEquals(2,
+	 * basicMaths.addNumber(1,1)), ()->assertEquals(0,basicMaths.addNumber(0, 0)),
+	 * ()->assertEquals(10,basicMaths.addNumber(1, 9)) ); }
+	 */	
+	
+	@Nested
+	@Tag("Development")
+	@DisplayName("Adding Number Test cases")
+	public class Addtestclass{
+		
+		@Test
+		@DisplayName("Adding postive number")
+		public void postiveNumberTest() {
+			assertEquals(2,basicMaths.addNumber(1, 1));
+		}
+		
+		@Test
+		@DisplayName("Adding negative number")
+		public void negativeNumberTest() {
+			assertEquals(-2,basicMaths.addNumber(-1, -1));
+		}
+		
 	}
 
 	@Test
-	@DisplayName("adding Two numbers")
-	public void addNumberTest() {
-		boolean serverUp=true;
-		assumeTrue(serverUp);
-		assertAll(
-			()->assertEquals(2, basicMaths.addNumber(1,1)),
-			()->assertEquals(0,basicMaths.addNumber(0, 0)),
-			()->assertEquals(10,basicMaths.addNumber(1, 9))	
-				);
-	}	
-
-	@Test
+	@Tag("Development")
 	@DisplayName("Testing subtract method")
 	public void subtractNumberTest() {
 		
@@ -43,17 +75,21 @@ class BasicMathsTest {
 	}
 	
 	@Test
-	public void findCircleAreaTest() {
-		
+	@Tag("Production")
+	public void findCircleAreaTest(TestInfo testInfo,TestReporter testReporter) {
+
 		assertEquals(314.0,basicMaths.findCircleArea(10),"should calculate circle area");
 	}
 	
-	@Test
-	@Disabled
-	public void throwAException() {
-		
-		assertThrows(ArithmeticException.class, ()-> basicMaths.divide(5,0),"should throw Arithmetic Exception");
-	}
 	
+	  @Test	  
+	  @RepeatedTest(3) 
+	  @Disabled
+	  @Tag("Production")
+	  public void throwAException(RepetitionInfo rinfo) {
+	  System.out.println(rinfo.getCurrentRepetition());
+	  assertThrows(ArithmeticException.class, ()->
+	  basicMaths.divide(5,0),"should throw Arithmetic Exception"); }
+	 
 	
 }
